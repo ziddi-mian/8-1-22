4324ffd92e70af5200e83e185be3e8a4fe76abb9
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-08 10:18:36 +0500

    js

--- script.js
+++ script.js
@@ -1265,3 +1265,32 @@ console.log(diff_months(dt1, dt2));
 dt1 = new Date("June 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2014 11:13:00");
 console.log(diff_months(dt1, dt2));
+
+
+
+
+
+
+// Write a JavaScript function to get time differences in years between two dates.   
+// Test Data :
+// dt1 = new Date("June 13, 2014 08:11:00");
+// dt2 = new Date("October 19, 2017 11:13:00");
+// console.log(diff_years(dt1, dt2));
+// 3
+
+function diff_years(dt2, dt1) 
+ {
+
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+   diff /= (60 * 60 * 24);
+  return Math.abs(Math.round(diff/365.25));
+   
+ }
+
+dt1 = new Date(2014,10,2);
+dt2 = new Date(2014,10,11);
+console.log(diff_years(dt1, dt2));
+
+dt1 = new Date("June 13, 2014 08:11:00");
+dt2 = new Date("October 19, 2017 11:13:00");
+console.log(diff_years(dt1, dt2));
